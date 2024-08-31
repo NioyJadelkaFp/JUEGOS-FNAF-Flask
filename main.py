e7e1f6bd0eb0ce59dc975cb928d647989df905a0
JADELKA / JDT <166043734+NioyJadelkaFp@users.noreply.github.com>
2024-08-31 13:26:06 -0300

    mejora de no archivo

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from JUEGOS.main import jg
 from PORTAFOLIO.main import pr
 
@@ -6,6 +6,10 @@ app = Flask(__name__)
 
 app.register_blueprint(jg, url_prefix='/juegos')
 app.register_blueprint(pr, url_prefix='/portafolio')
+
+@app.route('/')
+def si():
+    return redirect("/juegos/")
 
 if __name__ == '__main__':
     app.run(debug=True)
